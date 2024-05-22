f893a8fcd70ce8f07b3903e2e2d75e0ac69b12da
lroyero333 <lroyero@unab.edu.co>
2024-05-22 14:40:47 -0500

    Revert "project of redirect link if app is not install"
    This reverts commit 93e1b264337e5e699f1aa200b7d9f212fa60b644.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,19 @@
-import { Box, CircularProgress } from "@mui/material";
-import { useRouter } from "next/router";
+import Link from "next/link";
+import Layout from "../components/Layout";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-
   useEffect(() => {
-    const { id } = router.query;
-
-    window.location.href = `vitrineaapp://product/${id}`;
+    window.location.href = "vitrineaapp://";
     setTimeout(() => {
       window.location.href =
         "https://play.google.com/store/apps/details?id=com.sv.vitrinnea.ltda";
     }, 2000);
-  }, [router.query]);
+  }, []);
 
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <CircularProgress />
-    </Box>
+    <div>
+      <p>Redirigiendoo...</p>
+    </div>
   );
 }
